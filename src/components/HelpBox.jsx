08abab4736ccc181699afe1b042442e3053fd03e
mohititarch <mohititarch@gmail.com>
2025-01-16 06:41:10 +0530

    added p

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -6,6 +6,7 @@ function HelpBox({ title, text }) {
   return (
     <article className="help-box">
       <h2>{title}</h2>
+      <p>Something for nothing</p>
       <p>{text}</p>
     </article>
   );
